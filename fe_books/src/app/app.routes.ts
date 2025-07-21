643cdb4846f6d6bd361ad94fb323506798b6b351
maddieposhard <poshardmadeline@gmail.com>
2025-07-21 14:14:17 -0500

    added auth and no auth guards and applied them to routes

--- fe_books/src/app/app.routes.ts
+++ fe_books/src/app/app.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
-import { BookListComponent } from './book-list/book-list.component';
+import { BookListComponent } from './pages/book-list/book-list.component';
+import { authGuard } from './auth.guard';
+import { noAuthGuard } from './no-auth.guard';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: BookListComponent
-    }
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent),
+    canActivate: [noAuthGuard]
+  },
+  {
+    path: 'books',
+    loadComponent: () => import('./pages/book-list/book-list.component').then(m => m.BookListComponent),
+    canActivate: [authGuard]
+  }
 ];
---
+++ fe_books/src/app/pages/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthenticationService, private router: Router) { }
+
+  login() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (res: any) => {
+        console.log('Logged in with token:', res.token);
+        this.authService.setToken(res.token);
+        this.router.navigate(['/']);
+      },
+      error: (error: any) => {
+        console.error('Login error', error);
+      },
+    });
+  }
+}

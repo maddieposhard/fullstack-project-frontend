d94a1a8672e9683e59e033630e7a972b031cc07c
maddieposhard <poshardmadeline@gmail.com>
2025-08-04 14:58:42 -0500

    changed environment

--- fe_books/src/environments/environment.ts
+++ fe_books/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000'
+    production: true,
+    apiUrl: 'https://booksappmaddie.onrender.com'
+    // apiUrl: 'http://localhost:3000'
   };
   
